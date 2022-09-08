ef3bc0f2834f61e0f6c1250a3bfff6a3d9251d00
huynhviha1703@gmail.com <huynhviha1703@gmail.com>
2022-09-08 12:17:02 +0700

    note schema, model

--- index.js
+++ index.js
@@ -1,40 +1,83 @@
+const mongoose = require("mongoose")
 const express = require("express")
+const Note = require("./models/note")
+const middleware = require('./middleware')
+
+require('dotenv').config()
+
 const app = express()
 
+app.use(express.static('build'))
 app.use(express.json())
+app.use(middleware.requestLogger)
 
-const requestLogger = (request, response, next) => {
-    console.log('Method:', request.method)
-    console.log('Path:  ', request.path)
-    console.log('Body:  ', request.body)
-    console.log('---')
-    next()
-}
 
-const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
-}
 
-app.use(requestLogger)
-
-let notes = [{
-    name: "Nemo"
-}, {
-    name: "Damian"
-}]
-
-app.get('/', (request, response) => {
-    response.send("<h1>Nemo</h1>")
-})
 
 app.get('/api/notes', (request, response) => {
-    response.json(notes)
+    Note.find({}).then(notes => {
+        response.json(notes)
+    })
 })
 
-app.use(unknownEndpoint)
+app.post('/api/notes', (request, response) => {
+    const body = request.body
 
-const PORT = 3001
+    if (body.content === undefined) {
+        return response.status(400).json({ error: 'content missing' })
+    }
+
+    const note = new Note({
+        content: body.content,
+        important: body.important || false,
+        date: new Date(),
+    })
+
+    note.save().then(savedNote => {
+        response.json(savedNote)
+    })
+})
+
+app.get('/api/notes/:id', (request, response, next) => {
+    Note.findById(request.params.id)
+        .then(note => {
+            if (note) {
+                response.json(note)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
+})
+
+app.delete('/api/notes/:id', (request, response, next) => {
+    Note.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
+})
+
+app.put('/api/notes/:id', (request, response, next) => {
+    const body = request.body
+
+    const note = {
+        content: body.content,
+        important: body.important,
+    }
+
+    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+        .then(updatedNote => {
+            response.json(updatedNote)
+        })
+        .catch(error => next(error))
+})
+
+app.use(middleware.errorHandler)
+app.use(middleware.unknownEndpoint)
+
+const PORT = process.env.PORT
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
+    console.log(`Server is running on ${PORT}`)
 })
---
+++ middleware.js
@@ -0,0 +1,22 @@
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+    next(error)
+}
+
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+}
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+module.exports = { errorHandler, requestLogger, unknownEndpoint }
