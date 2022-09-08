2dc3afc6afe248387f3c7076450a4bf32e8c3f66
huynhviha1703@gmail.com <huynhviha1703@gmail.com>
2022-09-08 13:30:23 +0700

    user schema, model, control and login

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,11 +1,22 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
+const User = require('../models/user')
 
-notesRouter.get('/', (request, response) => {
-    Note.find({}).then(notes => {
-        response.json(notes)
-    })
-})
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+        return authorization.substring(7)
+    }
+    return null
+}
+
+notesRouter.get('/', async (request, response) => {
+    const notes = await Note
+        .find({}).populate('user', { username: 1, name: 1 })
+    response.json(notes)
+});
 
 notesRouter.get('/:id', (request, response, next) => {
     Note.findById(request.params.id)
@@ -19,20 +30,27 @@ notesRouter.get('/:id', (request, response, next) => {
         .catch(error => next(error))
 })
 
-notesRouter.post('/', (request, response, next) => {
+notesRouter.post('/', async (request, response) => {
     const body = request.body
+    const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: 'token missing or invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
 
     const note = new Note({
         content: body.content,
-        important: body.important || false,
-        date: new Date()
+        important: body.important === undefined ? false : body.important,
+        date: new Date(),
+        user: user._id
     })
 
-    note.save()
-        .then(savedNote => {
-            response.json(savedNote)
-        })
-        .catch(error => next(error))
+    const savedNote = await note.save()
+    user.notes = user.notes.concat(savedNote._id)
+    await user.save()
+
+    response.json(savedNote)
 })
 
 notesRouter.delete('/:id', (request, response, next) => {
--- utils/middleware.js
+++ utils/middleware.js
@@ -1,10 +1,18 @@
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
+    logger.error(error.message)
 
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' })
     } else if (error.name === 'ValidationError') {
         return response.status(400).json({ error: error.message })
+    } else if (error.name === 'JsonWebTokenError') {
+        return response.status(401).json({
+            error: 'invalid token'
+        })
+    } else if (error.name === 'TokenExpiredError') {
+        return response.status(401).json({
+            error: 'token expired'
+        })
     }
 
     next(error)
