84782112d5836a4421fb043f05179a615e69510c
vihahuynh <huynhviha1703@gmail.com>
2022-09-12 23:10:27 +0700

    controllers: gte product by id

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,40 +1,22 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'commonjs': true,
-        'es2021': true
-    },
-    'extends': 'eslint:recommended',
-    'overrides': [
-    ],
-    'parserOptions': {
-        'ecmaVersion': 'latest'
-    },
-    'rules': {
-        'indent': [
-            'error',
-            4
-        ],
-        'linebreak-style': [
-            'error',
-            'windows'
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'never'
-        ],
-        'eqeqeq': 'error',
-        'no-trailing-spaces': 'error',
-        'object-curly-spacing': [
-            'error', 'always'
-        ],
-        'arrow-spacing': [
-            'error', { 'before': true, 'after': true }
-        ],
-        'no-console': 0
-    }
-}
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: "eslint:recommended",
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": ["error", "windows"],
+    // semi: ["error", "never"],
+    eqeqeq: "error",
+    "no-trailing-spaces": "error",
+    "object-curly-spacing": ["error", "always"],
+    "arrow-spacing": ["error", { before: true, after: true }],
+    "no-console": 0,
+  },
+};
