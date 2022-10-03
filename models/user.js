8cdb994bf6b8f630d85a551918eab3f0bb9878dd
huynhviha1703@gmail.com <huynhviha1703@gmail.com>
2022-10-03 16:40:17 +0700

    update user model  - add delivery addresses

--- models/user.js
+++ models/user.js
@@ -92,6 +92,9 @@ const userSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: "Cart",
   },
+  deliveryAddresses: [{
+    address: String
+  }]
 });
 
 userSchema.methods.generateAuthToken = async function () {
