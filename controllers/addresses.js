7d8bbb239539d38e9c76df5e452f1416f3411bc4
vihahuynh <huynhviha1703@gmail.com>
2022-10-15 23:11:53 +0700

    update password reset

--- controllers/addresses.js
+++ controllers/addresses.js
@@ -1,59 +1,64 @@
-const addressRouter = require("express").Router()
-const Address = require("./../models/address")
+const addressRouter = require("express").Router();
+const Address = require("./../models/address");
 
 addressRouter.get("/", async (request, response, next) => {
-    try {
-        const addresses = await Address.find({ user: request.user.id })
-        response.json(addresses)
-    } catch (err) {
-        next(err)
-    }
-})
+  try {
+    const addresses = await Address.find({ user: request.user.id });
+    response.json(addresses);
+  } catch (err) {
+    next(err);
+  }
+});
 
 addressRouter.post("/", async (request, response, next) => {
-    try {
-        const newAddress = new Address({
-            ...request.body,
-            user: request.user.id,
-            isDefault: request.user?.deliveryAddresses?.length === 0 ? true : false
-        })
-        const returnedAddress = await newAddress.save()
-        response.status(201).json(returnedAddress)
-    } catch (err) {
-        next(err)
-    }
-})
+  try {
+    const addresses = await Address.find({ user: request.user.id });
+    const newAddress = new Address({
+      ...request.body,
+      user: request.user.id,
+      isDefault: addresses.length ? false : true,
+    });
+    const returnedAddress = await newAddress.save();
+    response.status(201).json(returnedAddress);
+  } catch (err) {
+    next(err);
+  }
+});
 
 addressRouter.patch("/:id", async (request, response, next) => {
-    try {
-        const address = await Address.findById(request.params.id)
-        if (!address) {
-            response.status(204).send()
-        }
-        if (address.user.toString() !== request.user.id) {
-            response.status(403).json({ err: "permission denied" })
-        }
-        const updatedAddress = await Address.findByIdAndUpdate(request.params.id, request.body, { new: true, runValidators: true })
-        response.json(updatedAddress)
-    } catch (err) {
-        next(err)
+  try {
+    const address = await Address.findById(request.params.id);
+    if (!address) {
+      response.status(204).send();
     }
-})
+    if (address.user.toString() !== request.user.id) {
+      response.status(403).json({ err: "permission denied" });
+    }
+    const updatedAddress = await Address.findByIdAndUpdate(
+      request.params.id,
+      request.body,
+      { new: true, runValidators: true }
+    );
+    response.json(updatedAddress);
+  } catch (err) {
+    next(err);
+  }
+});
 
 addressRouter.delete("/:id", async (request, response, next) => {
-    try {
-        const address = await Address.findById(request.params.id)
-        if (!address) {
-            response.status(204).send()
-        }
-        if (address.user.toString() !== request.user.id) {
-            response.status(403).json({ err: "permission denied" })
-        }
-        await Address.findByIdAndDelete(request.params.id)
-        response.status(204).send()
-    } catch (err) {
-        next(err)
+  try {
+    const address = await Address.findById(request.params.id);
+    if (!address) {
+      response.status(204).send();
     }
-})
+    if (address.user.toString() !== request.user.id) {
+      response.status(403).json({ err: "permission denied" });
+    }
+    await Address.findByIdAndDelete(request.params.id);
+    response.status(204).send();
+  } catch (err) {
+    next(err);
+  }
+});
 
-module.exports = addressRouter
+module.exports = addressRouter;
--- controllers/passwordReset.js
+++ controllers/passwordReset.js
@@ -1,60 +1,64 @@
-const passwordResetRouter = require("express").Router()
-const User = require("./../models/user")
-const Token = require("./../models/token")
-const crypto = require("crypto")
-const bcrypt = require("bcrypt")
-const sendEmail = require("../utils/sendEmail")
+const passwordResetRouter = require("express").Router();
+const User = require("./../models/user");
+const Token = require("./../models/token");
+const crypto = require("crypto");
+const bcrypt = require("bcrypt");
+const sendEmail = require("../utils/sendEmail");
 
 passwordResetRouter.post("/", async (request, response, next) => {
-    try {
-        const user = await User.findOne({ email: request.body.email })
-        if (!user) {
-            return response.status(404).json({ err: "No user found" })
-        }
-        const newToken = new Token({
-            user: user.id,
-            token: crypto.randomBytes(32).toString("hex"),
-        })
-        const returnedToken = await newToken.save()
-
-        const link = `${process.env.URL}/password-reset/${user._id}/${returnedToken.token}`
-        await sendEmail(user.email, "[Plantme] Please reset your password", link)
-        response.json({ message: "Password reset link had sent to user." })
-    } catch (err) {
-        next(err)
+  try {
+    const user = await User.findOne({ email: request.body.email });
+    if (!user) {
+      return response.status(404).json({ err: "No user found" });
     }
-})
+    const newToken = new Token({
+      user: user.id,
+      token: crypto.randomBytes(32).toString("hex"),
+    });
+    const returnedToken = await newToken.save();
+
+    const link = `${process.env.URL}/password-reset/${user._id}/${returnedToken.token}`;
+    await sendEmail(user.email, "[Plantme] Please reset your password", link);
+    response.json({ message: "Password reset link had sent to user." });
+  } catch (err) {
+    next(err);
+  }
+});
 
 passwordResetRouter.post("/:userId/:token", async (request, response, next) => {
-    try {
-        const user = await User.findById(request.params.userId)
-        if (!user) {
-            return response.status(400).json({ err: "Invalid link or expired - user" })
-        }
-        const token = await Token.findOne({
-            user: request.params.userId,
-            token: request.params.token
-        })
-        if (!token) {
-            return response.status(400).json({ err: "Invalid link or expired - token" })
-        }
-        const now = Date.now()
-        const tokenCreatedAt = token.createdAt.getTime()
-        // 01 hour
-        if (now - tokenCreatedAt < 3600000) {
-            return response.status(400).json({ err: "Invalid link or expired - token" })
-        }
-
-        const saltRounds = 10;
-        user.passwordHash = await bcrypt.hash(request.body.password, saltRounds);
-        await user.save()
-        await token.remove()
-        response.json({ message: "Password reset successfully." })
-
-    } catch (err) {
-        next(err)
+  try {
+    const user = await User.findById(request.params.userId);
+    if (!user) {
+      return response
+        .status(400)
+        .json({ err: "Invalid link or expired - user" });
     }
-})
+    const token = await Token.findOne({
+      user: request.params.userId,
+      token: request.params.token,
+    });
+    if (!token) {
+      return response
+        .status(400)
+        .json({ err: "Invalid link or expired - token" });
+    }
 
+    // const now = Date.now()
+    // const tokenCreatedAt = token.createdAt.getTime()
+    // 01 hour
+    // if (now - tokenCreatedAt < 3600000) {
+    //     return response.status(400).json({ err: "Invalid link or expired - token" })
+    // }
+    console.log(user.passwordHash);
+    const saltRounds = 10;
+    user.passwordHash = await bcrypt.hash(request.body.password, saltRounds);
+    await user.save();
+    console.log(user.passwordHash);
+    await token.remove();
+    response.json({ message: "Password reset successfully." });
+  } catch (err) {
+    next(err);
+  }
+});
 
-module.exports = passwordResetRouter
+module.exports = passwordResetRouter;
